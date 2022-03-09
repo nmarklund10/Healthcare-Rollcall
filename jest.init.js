b8d92c080a9f2fa5fce9b554178c6a4828239c59
nmarklund10 <nmarklund10@outlook.com>
2022-03-09 09:47:16 -0500

    load .env file for testing

--- jest.init.js
+++ jest.init.js
@@ -1,4 +1,7 @@
 import {config} from '@vue/test-utils'
-import {rootFuncs} from "./__mocks__/main.mock";
+import {rootFuncs} from './__mocks__/main.mock'
+import dotenv from 'dotenv'
 
 config.mocks = rootFuncs
+
+dotenv.config();
